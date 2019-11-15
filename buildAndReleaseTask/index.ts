e2db36d32ab89442c3001a96e9d38b0c50450544
Shrikrishna Singh <krishnasingh.ss30@gmail.com>
2019-11-15 16:39:16 +0530

    Add support to windows

--- buildAndReleaseTask/index.ts
+++ buildAndReleaseTask/index.ts
@@ -6,28 +6,30 @@ import fs = require('fs');
 const filepath = tl.getInput("filePath", true) || "";
 const token = tl.getInput("accessToken", true) || "";
 const riskThreshold = tl.getInput("riskThreshold", false);
+const os = tl.getVariable("Agent.OS");
+var appknoxPath = "";
 
-const appknoxCLIPath = path.join(__dirname, 'appknox')
-
-const appknoxPath1 = tl.which("appknox");
-if (!appknoxPath1) {
+if (os == "Windows_NT"){
+    const appknoxCLIPath = path.join(__dirname, 'appknox.exe');
+    tl.cp(appknoxCLIPath, "C:\\Program Files\\");
+    appknoxPath = "C:\\Program Files\\appknox.exe";
+} else {
+    const appknoxCLIPath = path.join(__dirname, 'appknox')
     fs.chmodSync(appknoxCLIPath, "755");
     tl.cp(appknoxCLIPath, "/usr/local/bin/");
+    appknoxPath = tl.which("appknox");
 }
-
-const appknoxPath2 = tl.which("appknox");
-
 
 async function upload(filepath: string, riskThreshold?: string) {
     try {
-        const appknoxUploader = tl.tool(appknoxPath2);
+        const appknoxUploader = tl.tool(appknoxPath);
         const xargs = tl.tool('xargs')
         appknoxUploader.arg("upload")
                         .arg(filepath)
                         .arg("--access-token")
                         .arg(token);
         if (riskThreshold) {
-            xargs.arg(appknoxPath2)
+            xargs.arg(appknoxPath)
                   .arg("cicheck")
                   .arg("--risk-threshold")
                   .arg(riskThreshold)
