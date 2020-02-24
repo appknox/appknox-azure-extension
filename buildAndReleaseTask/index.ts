e71f7a6885d19a9999114e53df1e030d20898ce0
Sharat M R <cosmosgenius@gmail.com>
2020-02-24 18:34:10 +0530

    Add all the binaries and downloader script

--- buildAndReleaseTask/index.ts
+++ buildAndReleaseTask/index.ts
@@ -1,26 +1,79 @@
 import tl = require('azure-pipelines-task-lib/task');
-import tr = require('azure-pipelines-task-lib/toolrunner');
 import path = require('path');
 import fs = require('fs');
+const pkg = require('./package.json');
+const download = require('download');
 
 const filepath = tl.getInput("filePath", true) || "";
 const token = tl.getInput("accessToken", true) || "";
-const riskThreshold = tl.getInput("riskThreshold", false);
-const os = tl.getVariable("Agent.OS");
-var appknoxPath = "";
+const riskThreshold: string = tl.getInput("riskThreshold", true) || "low";
+const os = tl.getVariable("Agent.OS"); // https://docs.microsoft.com/en-us/azure/devops/pipelines/build/variables?view=azure-devops&tabs=yaml#agent-variables
+const winBinary = "appknox-Windows-x86_64.exe";
+const darwinBinary = "appknox-Darwin-x86_64";
+const linuxBinary = "appknox-Linux-x86_64";
 
-if (os == "Windows_NT"){
-    const appknoxCLIPath = path.join(__dirname, 'appknox.exe');
-    tl.cp(appknoxCLIPath, "C:\\Program Files\\");
-    appknoxPath = "C:\\Program Files\\appknox.exe";
-} else {
-    const appknoxCLIPath = path.join(__dirname, 'appknox')
-    fs.chmodSync(appknoxCLIPath, "755");
-    tl.cp(appknoxCLIPath, "/usr/local/bin/");
-    appknoxPath = tl.which("appknox");
+const binaryVersion = pkg.binary;
+
+function downloadPath(binary: string) {
+    return `https://github.com/appknox/appknox-go/releases/download/${binaryVersion}/${binary}`;
 }
 
-async function upload(filepath: string, riskThreshold?: string) {
+function getBinaryName() {
+    if(os == "Windows_NT") {
+        return winBinary;
+    }
+    if(os == "Darwin") {
+        return darwinBinary;
+    }
+    if(os == "Linux") {
+        return linuxBinary;
+    }
+    throw Error("unsupported os " + os);
+}
+
+async function downloadBinary() {
+    const binName = getBinaryName()
+    const dPath = downloadPath(binName);
+    await download(dPath, path.join(__dirname, "binaries"));
+}
+
+function getAppknoxPath(): string {
+    if(os == "Windows_NT") {
+        return "C:\\Program Files\\appknox.exe";
+    }
+    if(os == "Darwin" || os == "Linux") {
+        return "/usr/local/bin/appknox";
+    }
+    throw Error("unsupported os " + os);
+}
+
+async function copyAppknox() {
+    const appknoxPath = getAppknoxPath();
+    const binPath = path.join(__dirname, 'binaries');
+    const winBin = path.join(binPath, winBinary)
+    const darwinBin = path.join(binPath, darwinBinary);
+    const linuxBin = path.join(binPath, linuxBinary);
+    if(fs.existsSync(appknoxPath)) {
+        return;
+    }
+    await downloadBinary();
+    if(os == "Windows_NT") {
+        return tl.cp(winBin, appknoxPath);
+    }
+    if(os == "Darwin") {
+        tl.cp(darwinBin, appknoxPath, "-f");
+        return fs.chmodSync(appknoxPath, "755");
+    }
+    if(os == "Linux") {
+        tl.cp(linuxBin, appknoxPath, "-f");
+        return fs.chmodSync(appknoxPath, "755");
+    }
+    throw Error("unsupported os " + os);
+}
+
+async function upload(filepath: string, riskThreshold: string) {
+    await copyAppknox();
+    const appknoxPath = getAppknoxPath();
     try {
         const appknoxUploader = tl.tool(appknoxPath);
         const xargs = tl.tool('xargs')
@@ -28,15 +81,13 @@ async function upload(filepath: string, riskThreshold?: string) {
                         .arg(filepath)
                         .arg("--access-token")
                         .arg(token);
-        if (riskThreshold) {
-            xargs.arg(appknoxPath)
-                  .arg("cicheck")
-                  .arg("--risk-threshold")
-                  .arg(riskThreshold)
-                  .arg("--access-token")
-                  .arg(token);
-            appknoxUploader.pipeExecOutputToTool(xargs);
-        }
+        xargs.arg(appknoxPath)
+                        .arg("cicheck")
+                        .arg("--risk-threshold")
+                        .arg(riskThreshold)
+                        .arg("--access-token")
+                        .arg(token);
+        appknoxUploader.pipeExecOutputToTool(xargs);
         const ret = await appknoxUploader.exec();
         return ret;
     }
